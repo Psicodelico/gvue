7dae9efb977969eb8ea7c79ffdd0ea15d40db8c4
Psicodelico <guosj921@163.com>
2020-04-12 21:51:02 +0800

    data binding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,19 @@
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png">
         <HelloWorld msg="Welcome to Your Vue.js App"/>
+        {{aaa}}
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-    name: 'Home',
+    name: "Home",
     components: {
         HelloWorld
-    }
-}
+    },
+    data: () => ({ aaa: 111 })
+};
 </script>
