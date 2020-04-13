a8b0b1f176d3f9a16cff2a5d1eb6b4dcca8c05dd
Psicodelico <guosj921@163.com>
2020-04-13 10:45:37 +0800

    鼠标悬停

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="about">
         <h1>This is an {{about}} page</h1>
+        <span v-bind:title="message">鼠标悬停</span>
     </div>
 </template>
 
 <script>
 export default {
     data: () => ({
+        message: "页面加载于 " + new Date().toLocaleString(),
         about: "about1"
     })
 };
