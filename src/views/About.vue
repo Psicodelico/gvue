6ca1ec930c43ebd387bef01d1c2635bf2e400979
Psicodelico <guosj921@163.com>
2020-04-13 10:52:58 +0800

    v-for :key

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,11 @@
     <div class="about">
         <h1>This is an {{about}} page</h1>
         <span v-bind:title="message">鼠标悬停</span>
+        <ol>
+            <li v-for="todo in todos" :key="todo.text">
+                {{ todo.text }}
+            </li>
+        </ol>
     </div>
 </template>
 
@@ -9,7 +14,8 @@
 export default {
     data: () => ({
         message: "页面加载于 " + new Date().toLocaleString(),
-        about: "about1"
+        about: "about1",
+        todos: [{ text: "js" }, { text: "ng" }, { text: "vue" }]
     })
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
         <HelloWorld msg="Welcome to Your Vue.js App" />
-        {{aaa}}
     </div>
 </template>
 
@@ -15,6 +14,6 @@ export default {
     components: {
         HelloWorld
     },
-    data: () => ({ aaa: 111 })
+    data: () => ({})
 };
 </script>
